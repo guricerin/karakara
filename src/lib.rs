389d77fc3405a691bc331968a3a3cea064260eb5
guricerin <gurizumi@gmail.com>
2019-05-22 12:01:33 +0000

    extern crate rand

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,4 @@
+extern crate rand;
+
 pub mod karakara;
 pub mod mokou;
--- src/mokou.rs
+++ src/mokou.rs
@@ -1,12 +1,15 @@
+use super::rand::{thread_rng, Rng};
+
 static MISSTEPS: &'static [&str] = &[
     "(⌒,_ゝ⌒ ) 何やこの厨パァ！！！",
     "(⌒,_ゝ⌒ ) 厨ポケの５、６匹でガタガタ言うなよ～！",
     "(⌒,_ゝ⌒ ) 重くないか？その名前。",
     "(⌒,_ゝ⌒ ) 何やこの深海魚ォ！",
+    "(⌒,_ゝ⌒ ) 見たかお前！降参読み降参じゃ！",
 ];
 
 pub fn mokou() {
-    let len = MISSTEPS.len();
-    let idx = len % 3;
+    let mut rng = thread_rng();
+    let idx: usize = rng.gen_range(0, MISSTEPS.len());
     println!("{}", MISSTEPS[idx]);
 }
